93b0fe67dab3348fb080a749142701398bf08239
GoTeX <Jozef.fallouh2@gmail.com>
2024-05-22 13:33:41 +0200

    added firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChs9IgxdPQA63nZANcSlZgEm-WBCClPW0",
+  authDomain: "media-cf8f1.firebaseapp.com",
+  projectId: "media-cf8f1",
+  storageBucket: "media-cf8f1.appspot.com",
+  messagingSenderId: "910373326352",
+  appId: "1:910373326352:web:f4b3520bbb632ffffcc495",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
